5e639880986a7d9b9af8e36202841548007023a1
Sebastian Raaphorst <sraaphorst@gmail.com>
2020-06-25 13:50:52 -0400

    Working example of full schedule.

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@
 from observation import Observations
 import resources
 
-from typing import Tuple
+from typing import List, Tuple
 from gurobipy import *
 
 
@@ -24,19 +24,22 @@ class Scheduler:
         self.timeslot_length = timeslot_length
         self.observations = Observations(timeslot_length)
 
-    def schedule(self) -> bool:
+    def schedule(self) -> List[Tuple[int, list]]:
         timeslot = 0
         scheduling = []
         while timeslot < self.timeslots:
             current_schedule = self.tick(timeslot)
-            scheduling.append((timeslot, [Scheduler.from_schedule_id(c) for c in current_schedule]))
+            scheduling.append((timeslot, current_schedule))#[Scheduler.from_schedule_id(c) for c in current_schedule]))
+            print("\n\n*** TIMESLOT %s ***" % timeslot)
             print("*** Schedule for timeslot %d: %s" % (timeslot, current_schedule))
-            print("Remaining times:")
+            print("*** Remaining times:")
             for id in range(self.observations.num_obs):
-                print("Obs %d, used_time=%f, obs_time=%f" % (id, self.observations.used_time[id], self.observations.obs_time[id]))
-            print()
+                print("Obs %d, used_time=%f, obs_time=%f" % (id,
+                                                             self.observations.used_time[id],
+                                                             self.observations.obs_time[id]))
+            print("*** ENDING TIMESLOT %s ***\n\n\n" % timeslot)
             timeslot += 1
-        return True
+        return scheduling
 
     @staticmethod
     def to_schedule_id(id: int, site: resources.Site) -> str:
@@ -61,7 +64,7 @@ class Scheduler:
     def do_work(self, scheduler_id: str):
         """
         Register work done on an observation.
-        :param id: the schedule ID of the observation
+        :param scheduler_id: the schedule ID of the observation
         """
         # We have used up one of the time slots.
         self.observations.used_time[Scheduler.from_schedule_id(scheduler_id)[0]] += self.timeslot_length
@@ -75,12 +78,12 @@ class Scheduler:
         :param timeslot: the active timeslot, from [0, timeslots).
         """
 
-        print("*** TIMESLOT %s ***" % timeslot)
+        print("*** BEGINNING TIMESLOT %s ***" % timeslot)
 
         self.observations.tick(timeslot)
 
         m = Model()
-        #m.setParam("OutputFlag", False)
+        m.setParam("OutputFlag", False)
 
         # Create all the decision variables and the constraints.
         vs = {}
@@ -124,7 +127,7 @@ class Scheduler:
         m.addConstr(sum(gs_vs) <= 1, "c_gn")
 
         # Create the objective function on which we wish to maximize.
-        #print("**** %s ****" % self.observations.priority)
+        # print("**** %s ****" % self.observations.priority)
         m.setObjective(sum([self.observations.priority[id] * vs[(id, res)] for (id, res) in vs.keys()]), GRB.MAXIMIZE)
 
         # The model is complete: call update on it to indicate that this is the case.
@@ -137,8 +140,8 @@ class Scheduler:
         m.optimize()
 
         # Now we determine the schedule for this tick and adjust the observation completion times appropriately.
-        print('\n\n*** ATTR ***')
-        m.printAttr('X')
+        # print('\n\n*** ATTR ***')
+        # m.printAttr('X')
 
         s_ids = m.getVars()
 
@@ -147,27 +150,9 @@ class Scheduler:
         for c in current_scheduling:
             self.do_work(c)
         return current_scheduling
-        #
-        # try:
-        #     ids = [int(var.VarName.split('_')[1:3]) for var in m.getVars()]
-        # except ValueError as err:
-        #     print('Internal illegal state: not a variable.\n"%s":'  % err)
-        #     exit(1)
-        #
-        # for id in ids:
-        #     self.observations.do_work(id)
-        #     if self.observations.is_done(id):
-        #         print("*** Observation %s has been completed." % id)
-        #     else:
-        #         print("*** Working on observation %s..." % id)
-        # else:
-        #     print("*** Nothing could be scheduled.")
 
-"""
-*** MAIN: DEFINE THE OBSERVATIONS AND KICK-OFF ***
-"""
-if __name__ == '__main__':
 
+def run_simulation1():
     # We schedule three time slots, each of 600 seconds.
     sched = Scheduler(2, 600)
 
@@ -224,8 +209,43 @@ if __name__ == '__main__':
     # Allocated time: 66000 s, obs time 60000
     sched.observations.add_obs('2', {0: {resources.Site.GN, resources.Site.GN}, 1: {resources.Site.GS}}, 66000, 60000)
 
-    sched.schedule()
+    return sched, sched.schedule()
 
-    print("*** RESULTS ***")
+
+def run_simulation2():
+    sched = Scheduler(8, 600)
+
+    # Shorthand
+    NS = {resources.Site.GN, resources.Site.GS}
+    N = {resources.Site.GN}
+    S = {resources.Site.GS}
+    E = set()
+
+    # 0
+    sched.observations.add_obs('1', {0: S,  1: E, 2: S,  3: NS, 4: NS, 5: E,  6: N,  7: NS}, 1200, 1200)
+    # 1
+    sched.observations.add_obs('3', {0: NS, 1: N, 2: NS, 3: E,  4: N,  5: E,  6: NS, 7: S},   500,  500)
+    # 2
+    sched.observations.add_obs('2', {0: N,  1: E, 2: N,  3: E,  4: NS, 5: S,  6: NS, 7: NS},  700,  700)
+    # 3
+    sched.observations.add_obs('1', {0: NS, 1: S, 2: NS, 3: NS, 4: S,  5: N,  6: NS, 7: E},   600,  600)
+    # 4
+    sched.observations.add_obs('2', {0: S,  1: N, 2: S,  3: E,  4: NS,  5: N, 6: S,  7: N}, 1800, 1800)
+    # 5
+    sched.observations.add_obs('3', {0: N,  1: E, 2: NS, 3: E,  4: E,   5: E, 6: N,  7: S}, 1200, 1200)
+
+    return sched, sched.schedule()
+
+
+"""
+*** MAIN: DEFINE THE OBSERVATIONS AND KICK-OFF ***
+"""
+if __name__ == '__main__':
+
+    sched, final_sched = run_simulation2()
+
+    print("*** SCHEDULE ***")
+    for s in range(len(final_sched)):
+        print("Time slot %d: %s" % (s, final_sched[s]))
     for id in range(sched.observations.num_obs):
         print("Obs %d done: %s" % (id, sched.observations.is_done(id)))
